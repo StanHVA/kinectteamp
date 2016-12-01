256759e50551500ddfa602489c46c080747e7ff3
Kübra <kozcann023@gmail.com>
2016-12-01 11:43:27 +0100

    ShinyApp - Bodyparts
    ShinyApp - Bodyparts

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,23 +1,13 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
-library(dplyr)
 library(ggvis)
-
 
 # Define UI for application that draws a histogram
 ui <- shinyUI((pageWithSidebar(
   div(),
   sidebarPanel(
-    fileInput('datfile'," "),
-    selectInput("")
+    fileInput('datfile', " "),
     selectInput('x', 'x:' ,'x'),
     selectInput('y', 'y:', 'y'),
     uiOutput("plot_ui")
@@ -89,4 +79,7 @@ server <- shinyServer(function(input, output, session) {
 })
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)                        
+
+
+
